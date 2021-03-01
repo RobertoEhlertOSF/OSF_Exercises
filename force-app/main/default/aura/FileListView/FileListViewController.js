6d28d6b3286dfe9fb6ba61d77d413ac4095a20ee
oldbetu <63211875+oldbetu@users.noreply.github.com>
2021-03-01 10:55:40 -0300

    Resolved comments

--- force-app/main/default/aura/FileListView/FileListViewController.js
+++ force-app/main/default/aura/FileListView/FileListViewController.js
@@ -31,11 +31,11 @@
                 var errors = response.getError();
                 if (errors) {
                     if (errors[0] && errors[0].message) {
-                        console.log("Error message: " + errors[0].message);                                    
+                        alert("Error message: " + errors[0].message);                                    
                     }
                 } 
                 else {
-                    console.log("Unknown Error");
+                    alert("Unknown Error");
                 }
             }
         });
